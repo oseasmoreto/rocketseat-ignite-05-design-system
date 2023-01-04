4ef972bd3e5208a591a9d325efe72d442d94ca87
oseasmoreto <oseas_moreto@hotmail.com>
2023-01-04 13:10:13 -0300

    Adiciona um componente de botao no story book

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '@oseasmoreto/react'
+
+export default {
+  title: 'Button',
+  component: Button,
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Enviar',
+  },
+}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+    children: 'Enviar',
+  },
+}
