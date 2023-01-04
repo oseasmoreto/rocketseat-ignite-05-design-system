1e94918388847164ec056cf7ff1b91dcdc079289
oseasmoreto <oseas_moreto@hotmail.com>
2023-01-04 10:32:45 -0300

    Instala o react para gerar os componentes

--- packages/react/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { colors } from '@oseasmoreto/tokens'
-
-export function HelloWorld() {
-  console.log(colors)
-}
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from '@oseasmoreto/tokens'
+
+export function App() {
+  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
+}
